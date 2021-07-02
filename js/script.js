da38fc2128945888a96a5d467e23b4f8689d493e
shamikhquraan1 <shamikhq@yahoo.com>
2021-07-02 07:26:36 +0300

    finally its worked

--- js/script.js
+++ js/script.js
@@ -15,58 +15,92 @@ function Locatplace(name,max,min,avg){
     this.hour= ['6am', '7am' , '8am','9am', '10am', '11 am', '12 pm ' , '1 pm ', '2 pm ' , '3pm ','4pm', '5pm', '6pm','7pm'];
     this.max=max;
     this.min=min;
+    this.arrC=[];
     this.avgC=function() {
     let customer=Math.ceil( avg * RandomSev(min,max));
     //console.log(getRandomInt(min,max));
     //console.log(customer);
+    this.arrC.push(customer);
     return customer;
-    
-    } ;
+    } 
     locatplaces.push(this);
   };
    
     Locatplace.prototype.render = function () {
-      let sum=0;
-      let trEl = document.createElement('tr');
-      if(this.min==23){
-          let tdEl3 = document.createElement('td');
-          tdEl3.textContent ='Seattle';
-          trEl.appendChild(tdEl3);
-        }else if(this.min==3){
-          let tdEl3 = document.createElement('td');
-          tdEl3.textContent ='Tokyo';
-          trEl.appendChild(tdEl3);
-        }else if(this.min==11){
-          let tdEl3 = document.createElement('td');
-          tdEl3.textContent ='Dubai';
-          trEl.appendChild(tdEl3);
-        }else if(this.min==20){
-          let tdEl3 = document.createElement('td');
-          tdEl3.textContent ='Paris';
-          trEl.appendChild(tdEl3);
-        } else if(this.min==2){
-          let tdEl3 = document.createElement('td');
-          tdEl3.textContent ='Lima';
-          trEl.appendChild(tdEl3);    }
-           else{
-              
-          let tdEl3 = document.createElement('td');
-          tdEl3.textContent =this.name;
-          trEl.appendChild(tdEl3);
-            
-          }
-      for(let i=0 ; i<this.hour.length ; i++){
-        let tdEl1 = document.createElement('td');
+      let sum = 0;
+      let total= 0 ;
+      let trEl = document.createElement("tr");
+      let tdEl3 = document.createElement("td");
+      tdEl3.textContent = this.name;
+      trEl.appendChild(tdEl3);
+      
+      let trE6 = document.createElement("tr");
+      let tdEl5 = document.createElement("td");
+      tdEl5.textContent = 'Total';
+      trE6.appendChild(tdEl5);
+      
+      for (let i = 0; i < this.hour.length; i++) {
+        let tdEl1 = document.createElement("td");
         tdEl1.textContent = this.avgC();
         trEl.appendChild(tdEl1);
-      sum+=this.avgC() ;
+        sum += this.avgC();
+        for(let j=0 ; j<locatplaces.length ; j++){
+          total=total+this.avgC();
+
+        } 
+       
+        let tdEl88 = document.createElement("td");
+        tdEl88.textContent = total;
+        trE6.appendChild(tdEl88);
       }
-    let tdEl2 = document.createElement('td');
-    tdEl2.textContent = sum;
-    trEl.appendChild(tdEl2);
-    tableEl.appendChild(trEl);
-    containerEl.appendChild(tableEl);
-    };
+      
+      let tdEl2 = document.createElement("td");
+      tdEl2.textContent = sum;
+      trEl.appendChild(tdEl2);
+      let tdEl88 = document.createElement("td");
+      tdEl88.textContent = total;
+      trE6.appendChild(tdEl88);
+
+
+
+
+
+
+
+
+      
+      tableEl.appendChild(trEl);
+      tableEl.appendChild(trE6);
+      containerEl.appendChild(tableEl);
+
+
+    }
+    
+
+    // Location.prototype.footer=function(){
+    // let sum1=0;
+    // let total=0;
+    // let trE2 = document.createElement("tr");
+    // let tdEl3 = document.createElement("td");
+    // tdEl3.textContent = 'total';
+    // trE2.appendChild(tdEl3);
+    // for(let i=0 ; i< this.hour.length ; i++)
+    // {
+    // for(let j=0 ; j<locatplaces.length ; j++){
+
+    //   let tdEl3 = document.createElement("td");
+    //   tdEl3.textContent = this.avgC();
+    //   trE2.appendChild(tdEl3); 
+    //  }
+    //  total+=sum1;
+    // }
+    // let tdEl2 = document.createElement("td");
+    // tdEl2.textContent = sum1;
+    // trEl.appendChild(tdEl2);
+    // tableEl.appendChild(trEl);
+    //     containerEl.appendChild(tableEl);
+    // }
+    
      
       let arr=[ 'loc/time ', '6am', '7am' , '8am','9am', '10am', '11 am', '12 pm ' , '1 pm ', '2 pm ' , '3pm ','4pm', '5pm', '6pm','7pm','Daily location total'];
       let containerEl = document.getElementById('container');
@@ -88,13 +122,19 @@ function Locatplace(name,max,min,avg){
       let paris =new Locatplace('paris',38,20,2.3);
       let lima =new Locatplace('lima',16,2,4.6);
       seattle.render();
+      tableEl.deleteRow(tableEl.rows.length - 1);
       tokyo.render();  
+      tableEl.deleteRow(tableEl.rows.length - 1);
+
       dubai.render();
+      tableEl.deleteRow(tableEl.rows.length - 1);
       paris.render();
+      tableEl.deleteRow(tableEl.rows.length - 1);
+
       lima.render();
 
       
-let myForm = document.getElementById('myForm');
+let myform = document.getElementById('myForm');
 myForm.addEventListener('submit', addRow);
 function addRow(event) {
   event.preventDefault();
@@ -104,7 +144,7 @@ function addRow(event) {
    let   miniNum=event.target.miniNum.value;
     let   avGnum = event.target.avGnum.value;
   let brazeel = new Locatplace(loCname,mXnumber,miniNum,avGnum);
-brazeel.render();
-}
+  tableEl.deleteRow(tableEl.rows.length - 1);
 
-  
+brazeel.render();
+};
